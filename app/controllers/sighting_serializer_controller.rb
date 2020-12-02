4a1f50e698e38363bd680f1fdf528312b844fbbb
Newland-A <ajn252@gmail.com>
2020-12-02 06:51:42 -0500

    Done.

---
+++ app/controllers/sighting_serializer_controller.rb
@@ -0,0 +1,46 @@
+class SightingSerializerController < ApplicationController
+
+  def initialize(sighting_object)
+    @sighting = sighting_object
+  end
+
+  def to_serialized_json
+    options = {
+      include: {
+        bird: {
+          only: [ :name, :species ]
+        },
+        location: {
+          only: [ :latitude, :longitude ]
+        }
+      },
+      except: [:updated_at],
+    }
+    @sighting.to_json(options)
+    # changed to above to improve readability and organization
+    # @sighting.to_json(:include => {
+    #   :bird => {
+    #     :only => [:name, :species]
+    #   },
+    #   :location => {
+    #     :only => [:latitude, :longitude]
+    #   }
+    # }, :except => [:updated_at])
+  end
+
+  # def index
+  #   sightings = Sighting.all
+  #   render json: sightings.to_json(:include => {
+  #     :bird => {:only => [:name, :species]},
+  #     :location => {:only => [:latitude, :longitude]}
+  #   }, :except => [:updated_at])
+  # end
+
+  # def show
+  #   sighting = Sighting.find_by(id: params[:id])
+  #   render json: sighting.to_json(:include => {
+  #     :bird => {:only => [:name, :species]},
+  #     :location => {:only => [:latitude, :longitude]}
+  #   }, :except => [:updated_at])
+  # end
+end
--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,21 @@
 class SightingsController < ApplicationController
+    
+    def index
+        sightings = Sighting.all
+        render json: SightingSerializer.new(sightings).to_serialized_json
+
+        # Removed this logic from the controller and moved it to a single method that can be called in this controller
+        # render json: sightings.to_json(:include => {
+        #   :bird => {:only => [:name, :species]},
+        #   :location => {:only => [:latitude, :longitude]}
+        # }, :except => [:updated_at])
+    end
+
     def show
         @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+        render json: SightingSerializer.new(sightings).to_serialized_json
+        # removed and added to serializer
+        # render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
     end
 end
+# Extraction complete! We've resolved the issue of keeping our controller clear of excess logic by moving it to a separate class.
